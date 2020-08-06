81ce8c1a8d908867332c3678764af55fdd4ae6d0
poohsky23 <poohsky23@P00A51603-002.CNS.LGCNS.COM>
2020-08-06 15:51:22 +0900

    함수를 이용한 최대 일 수 구하기

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,6 +3,12 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	public int getmaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
 
 	public static void main(String[] args) {
 
@@ -10,13 +16,11 @@ public class Calendar {
 		// 숫자를 입력받는다.
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		int month = scanner.nextInt();
 
-		// 달의 최대 일수를 배열에 담는다.
-		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
 		// 조건 문으로 판단 후 출력
-		System.out.printf("%d달은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
+		System.out.printf("%d달은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
 		scanner.close();
 	}
 }
