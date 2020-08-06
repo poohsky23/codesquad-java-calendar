098c7b3051994873f1f47a799b222d3d07c51133
poohsky23 <poohsky23@P00A51603-002.CNS.LGCNS.COM>
2020-08-06 15:40:33 +0900

    입력받은 달의 최대 일수 구하기

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,13 +1,22 @@
 package honux.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println(" 일   월   화   수    목    금    토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		// 숫자를 입력받는다.
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+
+		// 달의 최대 일수를 배열에 담는다.
+		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		// 조건 문으로 판단 후 출력
+		System.out.printf("%d달은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
+		scanner.close();
 	}
 }
