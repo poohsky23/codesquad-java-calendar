f816807c2ee9d2068dfa9918638d308f38ee320d
poohsky23 <poohsky23@P00A51603-002.CNS.LGCNS.COM>
2020-08-07 14:23:52 +0900

    요일 입력받아서 달력 출력

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,7 +1,5 @@
 package honux.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -22,32 +20,37 @@ public class Calendar {
 		return MAX_DAYS[month - 1];
 	}
 
-	/*
-	 * - 월을 입력하면 해당 월의 달력을 출력한다. - 달력의 모양은 1단계에서 작성한 모양으로 만든다. - 1일은 일요일로 정해도 무방하다.
-	 * - -1을 입력받기 전까지 반복 입력받는다. - 프롬프트를 출력한다.
-	 */
-
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("     <<%4d%2d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
-		
-		int maxDay = getmaxDaysOfMonth(year, month);
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
 
-		for (int i = 1; i <= maxDay; i++) {
+		int maxDay = getmaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = 7 - weekday;
+
+		// 첫번째 라인 요일에 맞춰 출력
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+		}
+		System.out.println();
+
+		// 두번째 라인부터 마지막 출력
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim) {
 				System.out.println();
 			}
 		}
 
 		System.out.println();
+		System.out.println();
 
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
 	}
 
 }
--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -3,8 +3,32 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Prompt {
+	
+	/*
+	 * @param week 요일명
+	 * @return 0~6 (0 = Sunday, 6 = Saturday)
+	 * 
+	 * */
 
-	private final static String PROMPT = "cal> ";
+	public int parseDay(String week) {
+		if (week.equals("SU")) {
+			return 0;
+		} else if (week.equals("MO")) {
+			return 1;
+		} else if (week.equals("TU")) {
+			return 2;
+		} else if (week.equals("WE")) {
+			return 3;
+		} else if (week.equals("TH")) {
+			return 4;
+		} else if (week.equals("FR")) {
+			return 5;
+		} else if (week.equals("SA")) {
+			return 6;
+		} else {
+			return 0;
+		}
+	}
 
 	public void runPrompt() {
 
@@ -12,17 +36,22 @@ public class Prompt {
 		Calendar cal = new Calendar();
 
 		int month = 1;
-		int year = 1;
+		int year = 2020;
+		int weekday = 0;
 
 		while (true) {
 			System.out.println("년도를 입력하세요.");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
-			
+
 			System.out.println("달을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			
+
+			System.out.println("첫째날의 요일을 입력하세요(SU, MO, TU, WE, TH, FR, SA).");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+
 			if (month == -1) {
 				break;
 			}
@@ -31,7 +60,7 @@ public class Prompt {
 				continue;
 			}
 
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, weekday);
 		}
 
 		System.out.println("Bye~");
