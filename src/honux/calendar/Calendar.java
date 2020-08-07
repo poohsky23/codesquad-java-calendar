e298b5b2fa5fbd8ba2e791779e5a1ef7c7bc07ca
poohsky23 <poohsky23@P00A51603-002.CNS.LGCNS.COM>
2020-08-07 14:53:47 +0900

    요일 계산 알고리즘 추가

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -2,8 +2,8 @@ package honux.calendar;
 
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
@@ -15,23 +15,26 @@ public class Calendar {
 
 	public int getmaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month - 1];
+			return LEAP_MAX_DAYS[month];
 		}
-		return MAX_DAYS[month - 1];
+		return MAX_DAYS[month];
 	}
 
-	public void printCalendar(int year, int month, int weekday) {
+	public void printCalendar(int year, int month) {
 		System.out.printf("     <<%4d%2d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
+		
+		int weekday = getWeekDay(year, month, 1);
+		
 		for (int i = 0; i < weekday; i++) {
 			System.out.print("   ");
 		}
 
 		int maxDay = getmaxDaysOfMonth(year, month);
 		int count = 7 - weekday;
-		int delim = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
 
 		// 첫번째 라인 요일에 맞춰 출력
 		for (int i = 1; i <= count; i++) {
@@ -53,4 +56,29 @@ public class Calendar {
 
 	}
 
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+
+		final int SWEEKDAY = 3; // 1970/01/01 = Thursday
+		
+		int count = 0;
+		
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+		
+		for (int i = 1; i < month; i++) {
+			int delta = getmaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		
+		count += day;
+		
+		int weekday = (count + SWEEKDAY) % 7;
+		
+		return weekday;
+	}
+	
+	
 }
--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -3,12 +3,12 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	
+
 	/*
 	 * @param week 요일명
 	 * @return 0~6 (0 = Sunday, 6 = Saturday)
 	 * 
-	 * */
+	 */
 
 	public int parseDay(String week) {
 		if (week.equals("SU")) {
@@ -37,30 +37,27 @@ public class Prompt {
 
 		int month = 1;
 		int year = 2020;
-		int weekday = 0;
 
 		while (true) {
-			System.out.println("년도를 입력하세요.");
+			System.out.println("년도를 입력하세요. (exit: -1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
+
+			if (year == -1) {
+				break;
+			}
 
 			System.out.println("달을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
 
-			System.out.println("첫째날의 요일을 입력하세요(SU, MO, TU, WE, TH, FR, SA).");
-			String str_weekday = scanner.next();
-			weekday = parseDay(str_weekday);
-
-			if (month == -1) {
-				break;
-			}
-
-			if (month > 12) {
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
+				System.out.println();
 				continue;
 			}
 
-			cal.printCalendar(year, month, weekday);
+			cal.printCalendar(year, month);
 		}
 
 		System.out.println("Bye~");
